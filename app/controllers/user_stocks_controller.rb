bcb4b6b0f946c2988e36c8704676b7db89eda2a2
Thomas Pietschmann <thomas@pietschi.com>
2021-01-14 21:21:09 +0100

    add delete stock option

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,4 +11,11 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio."
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    user_stock = UserStock.find_by(user: current_user, stock_id: params[:id])
+    user_stock.delete
+    flash[:notice] = "#{user_stock.stock.name} was removed from your portfolio"
+    redirect_to my_portfolio_path
+  end
 end
