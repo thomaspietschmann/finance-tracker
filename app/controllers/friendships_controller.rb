f480f22ac3021eeab84c8f5316f37478a5e208e6
Thomas Pietschmann <thomas@pietschi.com>
2021-01-17 12:44:33 +0100

    add/remove friends done

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,16 +1,24 @@
 class FriendshipsController < ApplicationController
 
   def destroy
-    friend = User.find(params[:id])
-    friendship = Friendship.find_by(user: current_user, friend: friend)
+    # friend = User.find(params[:id])
+    # friendship = Friendship.find_by(user: current_user, friend: friend)
+    friendship = current_user.friendships.find_by(friend_id: params[:id])
     friendship.delete
+    flash[:notice] = "Stopped following"
     redirect_to my_friends_path
   end
 
 
   def create
     friend = User.find(params[:friend])
-    current_user.friends << friend
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following user"
+    else
+      flash[:alert] = "something went wrong"
+    end
+    redirect_to my_friends_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:destroy, :create]
-  devise_for :users
+
   root 'welcome#index'
 
   get '/my_portfolio', to: 'users#my_portfolio'
